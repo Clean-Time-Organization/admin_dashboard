0cc8c32b53b3557f9408eb68bfa47e5d5b3620ec
Serhii Salii <s.saliy@infinitytechnologies.pro>
2023-10-30 16:52:25 +0200

    children props fix

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -10,7 +10,7 @@ interface Props {
   children: ReactNode
 }
 
-const MainLayout = ({ children }: Props) => {
+const MainLayout = ({ children }: Props): ReactNode => {
   const location = useLocation();
   if (location.pathname.toLowerCase().slice(0, 6) === '/login') return children
   return <Main>
