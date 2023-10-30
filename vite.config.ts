27eb516000248a2a68f964370b4c022c58ef9b10
Serhii Salii <s.saliy@infinitytechnologies.pro>
2023-10-30 13:19:31 +0200

    build test

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+
   server: {
     watch: {
       awaitWriteFinish: true
     },
     host: 'localhost',
-    port: 3000,
+    //port: 3000,
   },
-  plugins: [react()],
+
+  build: {
+    emptyOutDir: true,
+  }
 });
