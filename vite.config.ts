1e6082eb5a59a8853c37e207bb11945b55d5c247
Serhii Salii <s.saliy@infinitytechnologies.pro>
2023-10-30 18:06:05 +0200

    Module level directives cause errors when bundled, 'use client' was ignored

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import preserveDirectives from "rollup-plugin-preserve-directives";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    preserveDirectives(),
   ],
 
   server: {
