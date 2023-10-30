b3ca02ad6bdb1b5d2d153ef5f3c24c7cc8be4187
VShevcenko <v.shevchenko@infinitytechnologies.pro>
2023-10-30 17:01:04 +0200

    add alternative component

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -7,12 +7,12 @@ import {useLocation} from "react-router-dom";
 import {ReactNode} from "react";
 
 interface Props {
-  children: JSX.Element;
+  children: ReactNode;
 }
 
 const MainLayout = ({ children }: Props): ReactNode => {
   const location = useLocation();
-  if (location.pathname.toLowerCase().slice(0, 6) === '/login') return children
+  if (location.pathname.toLowerCase().slice(0, 6) === '/login') return children || <></>;
   return <Main>
     <Header />
     <Content>
