c2ae8d6f63a8d6bb15e9e836ac7dbc41fd132e10
Serhii Salii <s.saliy@infinitytechnologies.pro>
2023-11-15 01:26:08 +0300

    305

--- src/components/MainLayout/styled.ts
+++ src/components/MainLayout/styled.ts
@@ -1,9 +1,11 @@
 import { styled } from '@mui/system';
 
 const Content = styled('div')`
-  padding: 32px 0;
+  padding-top: 32px;
+  // padding-bottom: 75px;
   height: calc(100% - 64px);
   width: 1024px;
+  // height: 100%;
   position: relative;
 `;
 
