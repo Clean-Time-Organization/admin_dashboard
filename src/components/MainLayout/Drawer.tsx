c16556ef9de9af465aa39ce37a89b8ff0e91f338
Serhii Salii <s.saliy@infinitytechnologies.pro>
2023-11-20 23:06:39 +0300

    v1

---
+++ src/components/MainLayout/Drawer.tsx
@@ -0,0 +1,121 @@
+import {FC, memo} from "react";
+import {Box, Button, Drawer as MuiDrawer, Typography} from "@mui/material";
+import {Details} from "../Icons/Details";
+import {useAppSelector} from "../../store/hooks";
+
+type Props = {
+  width: number
+}
+
+export const Drawer: FC<Props> = memo(function CtDrawer({width}) {
+  const drawerData = useAppSelector(state => state.drawerData)
+
+  return (
+    <MuiDrawer
+      variant="persistent"
+      anchor="left"
+      open={true}
+      sx={{
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: {width},
+          boxSizing: "border-box",
+          marginTop: "64px",
+          paddingTop: "16px",
+        }
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          gap: "10px",
+          alignItems: "center",
+          padding: "12px 16px",
+          marginBottom: "16px",
+        }}
+      >
+        <Box
+          component="div"
+          sx={{
+            backgroundColor: "#EEF5FB",
+            width: "32px",
+            height: "32px",
+            borderRadius: '50%',
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#1A4C73",
+              leadingTrim: "both",
+              textEdge: "cap",
+              fontFamily: "Anek Latin",
+              fontSize: "14px",
+              fontStyle: "normal",
+              fontWeight: "600",
+              lineHeight: "120%",
+            }}
+          >
+            {drawerData.roundTitle}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            maxWidth: "180px",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#1F2937",
+              leadingTrim: "both",
+              textEdge: "cap",
+              fontFamily: "Anek Latin",
+              fontSize: "14px",
+              fontStyle: "normal",
+              fontWeight: "600",
+              lineHeight: "normal",
+              marginBottom: "12px",
+              whiteSpace: "nowrap",
+              textOverflow: "ellipsis",
+              overflow: "hidden",
+            }}
+          >
+            {drawerData.title}
+          </Typography>
+          <Typography
+            sx={{
+              color: "#474A58",
+              leadingTrim: "both",
+              textEdge: "cap",
+              fontFamily: "Anek Latin",
+              fontSize: "12px",
+              fontStyle: "normal",
+              fontWeight: "400",
+              lineHeight: "135%",
+            }}
+          >
+            {drawerData.subTitle}
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          paddingLeft: "16px",
+          paddingRight: "16px",
+        }}
+      >
+        <Button
+          variant="drawer"
+          startIcon={<Details/>}
+          style={{
+            backgroundColor: "#2E8DC8",
+          }}
+        >
+          Details
+        </Button>
+      </Box>
+    </MuiDrawer>
+  )
+})
--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -5,8 +5,7 @@ import {useLocation, Link, matchPath, useNavigate} from "react-router-dom";
 import {ReactNode, useEffect, useState} from "react";
 import {
   AppBar,
-  Box, Button, Drawer,
-  IconButton,
+  Box, IconButton,
   Menu,
   MenuItem, ThemeProvider,
   Toolbar, Typography
@@ -21,7 +20,7 @@ import {useAppDispatch, useAppSelector} from "../../store/hooks";
 import { Snackbar } from '../Snackbar/Snackbar';
 import { setNotification } from '../../store/features/notification';
 import {setBreadCrumbsData} from "../../store/features/breadCrumbsDataSlice";
-import {Details} from "../Icons/Details";
+import {Drawer} from "./Drawer";
 
 const MainLayout = ({children}: {children: ReactNode}) => {
   const dispatch = useAppDispatch()
@@ -30,7 +29,6 @@ const MainLayout = ({children}: {children: ReactNode}) => {
   const {signOut} = useAuth()
   const navigate = useNavigate()
   const authData = useAppSelector(state => state.authData)
-  const drawerData = useAppSelector(state => state.drawerData)
   const {notificationMessage, notificationType} = useAppSelector(state => state.notification)
   const [showToolBar, setShowToolBar] = useState(true)
   const [showDrawer, setShowDrawer] = useState(false)
@@ -308,129 +306,16 @@ const MainLayout = ({children}: {children: ReactNode}) => {
           }}
         >
           {showDrawer &&
-            <Drawer
-              variant="persistent"
-              anchor="left"
-              open={true}
-              sx={{
-                // width: drawerWidth,
-                flexShrink: 0,
-                "& .MuiDrawer-paper": {
-                  width: drawerWidth,
-                  boxSizing: "border-box",
-                  marginTop: "64px",
-                  paddingTop: "16px",
-                }
-              }}
-            >
-              <Box
-                sx={{
-                  display: "flex",
-                  gap: "10px",
-                  alignItems: "center",
-                  padding: "12px 16px",
-                  marginBottom: "16px",
-                }}
-              >
-                <Box
-                  component="div"
-                  sx={{
-                    backgroundColor: "#EEF5FB",
-                    width: "32px",
-                    height: "32px",
-                    borderRadius: '50%',
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <Typography
-                    sx={{
-                      color: "#1A4C73",
-                      leadingTrim: "both",
-                      textEdge: "cap",
-                      fontFamily: "Anek Latin",
-                      fontSize: "14px",
-                      fontStyle: "normal",
-                      fontWeight: "600",
-                      lineHeight: "120%",
-                    }}
-                  >
-                    {drawerData.roundTitle}
-                  </Typography>
-                </Box>
-                <Box
-                  sx={{
-                    maxWidth: "180px",
-                  }}
-                >
-                  <Typography
-                    sx={{
-                      color: "#1F2937",
-                      leadingTrim: "both",
-                      textEdge: "cap",
-                      fontFamily: "Anek Latin",
-                      fontSize: "14px",
-                      fontStyle: "normal",
-                      fontWeight: "600",
-                      lineHeight: "normal",
-                      marginBottom: "12px",
-                      whiteSpace: "nowrap",
-                      textOverflow: "ellipsis",
-                      overflow: "hidden",
-                    }}
-                  >
-                    {drawerData.title}
-                  </Typography>
-                  <Typography
-                    sx={{
-                      color: "#474A58",
-                      leadingTrim: "both",
-                      textEdge: "cap",
-                      fontFamily: "Anek Latin",
-                      fontSize: "12px",
-                      fontStyle: "normal",
-                      fontWeight: "400",
-                      lineHeight: "135%",
-                    }}
-                  >
-                    {drawerData.subTitle}
-                  </Typography>
-                </Box>
-              </Box>
-              <Box
-                sx={{
-                  paddingLeft: "16px",
-                  paddingRight: "16px",
-                }}
-              >
-                <Button
-                  variant="drawer"
-                  startIcon={<Details/>}
-                  style={{
-                    backgroundColor: "#2E8DC8",
-                  }}
-                >
-                  Details
-                </Button>
-              </Box>
-            </Drawer>
+            <Drawer width={drawerWidth}></Drawer>
           }
-          {
-            showDrawer ?
-              <Content
-                style={{
-                  // paddingLeft: "`${drawerWidth}px`",
-                  paddingLeft: "255px",
-                }}
-              >
-                {children}
-              </Content>
-              :
-              <Content>
-                {children}
-              </Content>
-          }
+          <Content
+            style={showDrawer ?
+              {paddingLeft: `${drawerWidth}px`}
+              : {}
+            }
+          >
+            {children}
+          </Content>
           { notificationMessage && notificationType &&
             <Snackbar text={notificationMessage} type={notificationType} />
           }
