7e79042643a4b2644c9fd1854be7b4a1590a61f4
Serhii Salii <s.saliy@infinitytechnologies.pro>
2023-11-22 18:15:57 +0300

    v1

---
+++ src/pages/Laundry/CreateLaundry.tsx
@@ -0,0 +1,97 @@
+import { useNavigate } from "react-router-dom";
+import { CreationPanel } from "../../components/CreationPanel/CreationPanel";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { UserForm } from "../../types/user";
+import httpClient from "../../services/HttpClient";
+import { useAppDispatch } from "../../store/hooks";
+import { setNotification } from "../../store/features/notification";
+
+const CreateLaundry = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const [totalSteps, setTotalSteps] = useState(3);
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const { control, watch, setValue, handleSubmit, setError, trigger } = useForm<UserForm>({
+    mode: 'onTouched',
+    defaultValues: {
+        role: 'POS',
+        password: '',
+    },
+  });
+
+  const cancelEvent = () => {
+    navigate('/laundries');
+  };
+
+  const handleCreate: SubmitHandler<UserForm> = async (values) => {
+    await httpClient.post(
+      '/laundry',
+      {...values, phone_number: '+966' + values.phone_number}
+    ).then(response => {
+      if (response.status === 200) {
+        navigate('/staff');
+        dispatch(setNotification({
+          notificationMessage: 'User is successfully created',
+          notificationType: 'success',
+        }));
+      }
+    }).catch(error => {
+      if (error.response.data.detail instanceof Array) {
+        error.response.data.detail.forEach((item: any) => {
+          setError(item.loc[1], { type: 'validate', message: item.msg + ''} );
+        });
+      } else if (Object.keys(error.response.data.detail)[0] === 'email') {
+        setError('email', { type: 'validate', message: Object.values(error.response.data.detail)[0] + ''} );
+      } else if (Object.keys(error.response.data.detail)[0] === 'full_name') {
+        setError('full_name', { type: 'validate', message: Object.values(error.response.data.detail)[0] + ''} );
+      } else if (Object.keys(error.response.data.detail)[0] === 'phone_number') {
+        setError('phone_number', { type: 'validate', message: Object.values(error.response.data.detail)[0] + ''} );
+      } else {
+        dispatch(setNotification({
+          notificationMessage: error.response.data.detail + '',
+          notificationType: 'error',
+        }));
+      }
+      // then - else => set global error message if smth went wrong, otherwise - success
+    });
+  };
+
+  return <CreationPanel
+    stepperTitle="Create Laundry"
+    totalSteps={totalSteps}
+    currentStep={currentStep}
+    cancelEvent={cancelEvent}
+  >
+    <>
+      {
+        // currentStep === 1 &&
+        //   <StepRole list={roleProperties} setValue={setValue} selectedValue={watchRole} setCurrentStep={() => setCurrentStep(currentStep + 1)} />
+      }
+      {
+        // currentStep === 2 &&
+        //   <StepLaundryInfo
+        //     control={control}
+        //     watch={watch}
+        //     setValue={setValue}
+        //     toPreviousStep={() => setCurrentStep(currentStep - 1)}
+        //     toNextStep={() => setCurrentStep(currentStep + 1)}
+        //   />
+      }
+      {
+        // currentStep === 3 &&
+        //   <StepUserDetails
+        //     control={control}
+        //     watch={watch}
+        //     setValue={setValue}
+        //     toPreviousStep={() => setCurrentStep(currentStep - 1)}
+        //     onCreate={handleSubmit(handleCreate)}
+        //     trigger={trigger}
+        //   />
+      }
+    </>
+  </CreationPanel>
+}
+
+export { CreateLaundry };
