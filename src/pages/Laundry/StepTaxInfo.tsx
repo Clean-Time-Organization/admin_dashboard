8f4332b4f3a19144bb1e04b6613350317df0071c
Serhii Salii <s.saliy@infinitytechnologies.pro>
2023-11-23 17:13:16 +0300

    v1

--- src/pages/Laundry/StepTaxInfo.tsx
+++ src/pages/Laundry/StepTaxInfo.tsx
@@ -1,10 +1,13 @@
-import { FC} from "react";
-import {BasicButtonLong, LinkButtonLong} from "../../components/Button/Buttons";
+import {FC, useRef, useState} from "react";
+import {BasicButtonLong, LinkButton, LinkButtonLong} from "../../components/Button/Buttons";
 import {Control, Controller, UseFormTrigger, UseFormWatch} from "react-hook-form";
 import {BlockSubtitle, BlockTitle, ButtonLine, StepBase, StepBaseInternal, StepSubtitle, StepTitle, Titles } from "./styled";
 import {LaundryForm} from "./CreateLaundry";
 import {InputBase} from "../../components/InputBase/InputBase";
 import {FieldErrors} from "react-hook-form/dist/types/errors";
+import {Box, Typography} from "@mui/material";
+import {Document} from "../../components/Icons/Document";
+import {Attach} from "../../components/Icons/Attach";
 
 interface IStepLaundryInfoProps {
   readonly control: Control<LaundryForm>
@@ -15,7 +18,13 @@ interface IStepLaundryInfoProps {
 }
 
 const StepTaxInfo: FC<IStepLaundryInfoProps> = ({control, errors, trigger, toPreviousStep, onCreate}) => {
-  const handleNextStep = () => {
+  const vatFileRef = useRef<HTMLInputElement>(null)
+  const crFileRef = useRef<HTMLInputElement>(null)
+
+  const[vatFile, setVatFile] = useState('')
+  const[crFile, setCrFile] = useState('')
+
+  const handleCreate = () => {
     trigger()
     const stepFields = ['vat_number', 'cr_number']
     const errorFields = Object.keys(errors)
@@ -24,6 +33,14 @@ const StepTaxInfo: FC<IStepLaundryInfoProps> = ({control, errors, trigger, toPre
     if (stepIsValid) {
       onCreate()
     }
+  }
+
+  const showUploadVatFile = () => {
+
+  }
+
+  const showUploadCrFile = () => {
+
   }
 
   return <StepBase>
@@ -36,51 +53,196 @@ const StepTaxInfo: FC<IStepLaundryInfoProps> = ({control, errors, trigger, toPre
         <BlockTitle>Vat Number</BlockTitle>
         <BlockSubtitle>Please indicate the VAT number for the laundry. Upload a file up to 10 MB.</BlockSubtitle>
       </Titles>
-      <Controller
-        control={control}
-        name="vat_number"
-        render={({ field: { ref, ...field }, fieldState: { error } }) => (
-          <InputBase
-            autoFocus
-            label={'VAT Number'}
-            error={error !== undefined}
-            errorText={error?.type === 'required' && 'Please enter the VAT Number' ||
-              error && error?.message
-            }
-            autoComplete={'off'}
-            {...field}
-          />
-        )}
-        rules={{
-          required: true,
+      <Box
+        display="flex"
+        justifyContent="flex-start"
+        sx={{
+          flexDirection: "row",
         }}
-      />
+      >
+        <Box
+          maxWidth={466}
+          minWidth={466}
+        >
+          <Controller
+            control={control}
+            name="vat_number"
+            render={({ field: { ref, ...field }, fieldState: { error } }) => (
+              <InputBase
+                autoFocus
+                label={'VAT Number'}
+                error={error !== undefined}
+                errorText={error?.type === 'required' && 'Please enter the VAT Number' ||
+                  error && error?.message
+                }
+                autoComplete={'off'}
+                {...field}
+                sx={{
+                  // maxWidth: "466px"
+                }}
+              />
+            )}
+            rules={{
+              required: true,
+            }}
+          />
+        </Box>
+        {vatFile ?
+            <Box>
+                <Box
+                    sx={{
+                      paddingLeft: "32px",
+                      paddingTop: "17px",
+                    }}
+                >
+                    <Document />
+                </Box>
+                <Box
+                    sx={{
+                      display: "flex",
+                      alignItems: "left",
+                      paddingLeft: "8px",
+                      paddingTop: "18px",
+                      maxWidth: "86px",
+                      minWidth: "86px",
+                    }}
+                >
+                    <Typography
+                        sx={{
+                          color: "#656873",
+                          textAlign: "center",
+                          leadingTrim: "both",
+                          textEdge: "cap",
+                          fontFamily: "Anek Latin",
+                          fontSize: "14px",
+                          fontStyle: "normal",
+                          fontWeight: "500",
+                          lineHeight: "130%",
+                          letterSpacing: "0.28px",
+                          whiteSpace: "nowrap",
+                          textOverflow: "ellipsis",
+                          overflow: "hidden",
+                        }}
+                    >
+                      {vatFile}
+                    </Typography>
+                </Box>
+            </Box>
+          :
+            <Box
+              sx={{
+                paddingLeft: "10px",
+                paddingTop: "8px",
+              }}
+            >
+              <LinkButton
+                onClick={showUploadVatFile}
+                preTextIcon={<Attach />}
+                pretext="true"
+              >
+                Upload file
+              </LinkButton>
+            </Box>
+        }
+        <input ref={vatFileRef} type="file" style={{ display: "none" }}/>
+      </Box>
       <Titles>
         <BlockTitle>CR Number</BlockTitle>
         <BlockSubtitle>Please indicate the CR number for the laundry. Upload a file up to 10 MB.</BlockSubtitle>
       </Titles>
-      <Controller
-        control={control}
-        name="cr_number"
-        render={({ field: { ref, ...field }, fieldState: { error } }) => (
-          <InputBase
-            label={'CR Number'}
-            error={error !== undefined}
-            errorText={error?.type === 'required' && 'Please enter the CR Number' ||
-              error && error?.message
-            }
-            autoComplete={'off'}
-            {...field}
-          />
-        )}
-        rules={{
-          required: true,
+      <Box
+        display="flex"
+        justifyContent="flex-start"
+        sx={{
+          flexDirection: "row",
         }}
-      />
+      >
+        <Box
+          maxWidth={466}
+          minWidth={466}
+        >
+          <Controller
+            control={control}
+            name="cr_number"
+            render={({ field: { ref, ...field }, fieldState: { error } }) => (
+              <InputBase
+                label={'CR Number'}
+                error={error !== undefined}
+                errorText={error?.type === 'required' && 'Please enter the CR Number' ||
+                  error && error?.message
+                }
+                autoComplete={'off'}
+                {...field}
+              />
+            )}
+            rules={{
+              required: true,
+            }}
+          />
+        </Box>
+        {crFile ?
+          <Box>
+            <Box
+              sx={{
+                paddingLeft: "32px",
+                paddingTop: "17px",
+              }}
+            >
+              <Document />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "left",
+                paddingLeft: "8px",
+                paddingTop: "18px",
+                maxWidth: "86px",
+                minWidth: "86px",
+              }}
+            >
+              <Typography
+                sx={{
+                  color: "#656873",
+                  textAlign: "center",
+                  leadingTrim: "both",
+                  textEdge: "cap",
+                  fontFamily: "Anek Latin",
+                  fontSize: "14px",
+                  fontStyle: "normal",
+                  fontWeight: "500",
+                  lineHeight: "130%",
+                  letterSpacing: "0.28px",
+                  whiteSpace: "nowrap",
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                }}
+              >
+                {vatFile}
+              </Typography>
+            </Box>
+          </Box>
+          :
+          <Box
+            sx={{
+              paddingLeft: "10px",
+              paddingTop: "8px",
+            }}
+          >
+            <LinkButton
+              onClick={showUploadCrFile}
+              preTextIcon={<Attach />}
+              pretext="true"
+            >
+              Upload file
+            </LinkButton>
+          </Box>
+        }
+        <input ref={crFileRef} type="file" style={{ display: "none" }}/>
+      </Box>
     </StepBaseInternal>
     <ButtonLine>
       <LinkButtonLong onClick={toPreviousStep}>Previous step</LinkButtonLong>
-      <BasicButtonLong onClick={onCreate}>Create laundry</BasicButtonLong>
+      <BasicButtonLong onClick={handleCreate}>Create laundry</BasicButtonLong>
     </ButtonLine>
   </StepBase>;
 }
